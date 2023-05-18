0c089b2d29a5fcc13562916f2b33bdaea49bf8bd
Piotr Michalski <pipi6620@gmail.com>
2023-05-18 17:46:56 +0200

    added a container for form

--- src/Form/index.js
+++ src/Form/index.js
@@ -13,47 +13,49 @@ const Form = ({ calculateResult, result }) => {
     };
 
     return (
-        <form className="form" onSubmit={onFormSubmit}>
-            <fieldset className="form__fieldset">
-                <legend className="form__legend">Kalkulator PLN na inną walutę</legend>
-                <ul className="form__list">
-                    <li className="form__listItem">
-                        <label>Wpisz ilość PLN</label>
-                        <input
-                            type="number"
-                            required
-                            min="0"
-                            step="0.01"
-                            className="form__inputBox"
-                            value={amount}
-                            onChange={({ target }) => setAmount(target.value)}
-                        />
-                    </li>
-                    <li className="form__listItem">
-                        <label>Wybierz walutę</label>
-                        <select
-                            name="currency"
-                            className="form__inputBox"
-                            value={currency}
-                            onChange={({ target }) => setCurrency(target.value)}
-                        >
-                            {currencies.map(currency => (
-                                <option
-                                    key={currency.name}
-                                    value={currency.name}
-                                >
-                                    {currency.fullName}
-                                </option>
-                            ))}
-                        </select>
-                    </li>
-                    <li className="form__listItem">
-                        <button type="submit" className="form__button">Oblicz</button>
-                    </li>
-                </ul>
-                <Result result={result} />
-            </fieldset>
-        </form>
+        <div className="container">
+            <form className="form" onSubmit={onFormSubmit}>
+                <fieldset className="form__fieldset">
+                    <legend className="form__legend">Kalkulator PLN na inną walutę</legend>
+                    <ul className="form__list">
+                        <li className="form__listItem">
+                            <label>Wpisz ilość PLN</label>
+                            <input
+                                type="number"
+                                required
+                                min="0"
+                                step="0.01"
+                                className="form__inputBox"
+                                value={amount}
+                                onChange={({ target }) => setAmount(target.value)}
+                            />
+                        </li>
+                        <li className="form__listItem">
+                            <label>Wybierz walutę</label>
+                            <select
+                                name="currency"
+                                className="form__inputBox"
+                                value={currency}
+                                onChange={({ target }) => setCurrency(target.value)}
+                            >
+                                {currencies.map(currency => (
+                                    <option
+                                        key={currency.name}
+                                        value={currency.name}
+                                    >
+                                        {currency.fullName}
+                                    </option>
+                                ))}
+                            </select>
+                        </li>
+                        <li className="form__listItem">
+                            <button type="submit" className="form__button">Oblicz</button>
+                        </li>
+                    </ul>
+                    <Result result={result} />
+                </fieldset>
+            </form>
+        </div>
     )
 };
 
