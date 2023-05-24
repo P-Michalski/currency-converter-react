a5b701dfca42cc2dd6e255ce02135487ca10412c
Piotr Michalski <pipi6620@gmail.com>
2023-05-24 15:59:47 +0200

    new colors added

--- src/theme.js
+++ src/theme.js
@@ -5,5 +5,7 @@ export const theme = {
         blizzardBlue: "hsl(182, 71%, 74%)",
         turquoise: "hsl(182, 71%, 54%);",
         bunting: "#101246",
+        alzarinCrimson: "#E32636",
+        tundora: "#474747",
     },
 };
