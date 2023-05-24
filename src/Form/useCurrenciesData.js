1f960ab855743ddd20e35082081944f744f0611b
Piotr Michalski <pipi6620@gmail.com>
2023-05-24 16:00:28 +0200

    useCurrenciesData hook added

---
+++ src/Form/useCurrenciesData.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const useCurrenciesData = () => {
+    const [currenciesData, setCurrenciesData] = useState(
+        {
+            phase: "loading",
+        }
+    );
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const response = await axios.get('https://api.exchangerate.host/latest?base=PLN');
+                const date = await response.data.date;
+                const rates = await response.data.rates;
+                setCurrenciesData(
+                    {
+                        phase: "success",
+                        date,
+                        rates,
+                    }
+                );
+            } catch (error) {
+                setCurrenciesData({ phase: "error" });
+            }
+        };
+        setTimeout(getData, 1000);
+    }, []);
+
+    return currenciesData;
+};
+
