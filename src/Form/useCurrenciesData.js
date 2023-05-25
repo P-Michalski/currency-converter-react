ea87a2fabe26beb9576fa88bbe7bb12390c0e9d3
Piotr Michalski <pipi6620@gmail.com>
2023-05-25 13:23:36 +0200

    moved API URL to a const

--- src/Form/useCurrenciesData.js
+++ src/Form/useCurrenciesData.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+const exchangeRateURL = 'https://api.exchangerate.host/latest?base=PLN';
+
 export const useCurrenciesData = () => {
     const [currenciesData, setCurrenciesData] = useState({
         status: "loading",
@@ -9,7 +11,7 @@ export const useCurrenciesData = () => {
     useEffect(() => {
         const getData = async () => {
             try {
-                const response = await axios.get('https://api.exchangerate.host/latest?base=PLN');
+                const response = await axios.get(exchangeRateURL);
                 const date = await response.data.date;
                 const rates = await response.data.rates;
                 setCurrenciesData({
